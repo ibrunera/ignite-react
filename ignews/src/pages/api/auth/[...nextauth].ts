eff6837f4e267f973cb7cf853efd6075091b557e
ibrunera <bteixeira010@gmail.com>
2021-03-23 00:19:35 -0300

    faunadb, users collection

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,37 @@ export default NextAuth({
       scope:'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user,acoount, profile){
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: user.email }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )   
+          )
+        )
+        return true
+      } catch (error) {
+        return false
+      }
+
+    }
+  }
 })
